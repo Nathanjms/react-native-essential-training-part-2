203faec266ccb7ab1e8ebf1be510ad2f5e63e31c
Nathan James <nathanjms@outlook.com>
2021-03-13 15:23:57 +0000

    Styling

--- src/components/DealItem.js
+++ src/components/DealItem.js
@@ -10,12 +10,14 @@ class DealItem extends Component {
   render() {
     const {deal} = this.props;
     return (
-      <View>
+      <View style={styles.item}>
         <Image source={{uri: deal.media[0]}} style={styles.image} />
-        <View>
-          <Text>{deal.title}</Text>
-          <Text>{priceDisplay(deal.price)}</Text>
-          <Text>{deal.cause.name}</Text>
+        <View style={styles.info}>
+          <Text style={styles.title}>{deal.title}</Text>
+          <View style={styles.footer}>
+            <Text style={styles.cause}>{deal.cause.name}</Text>
+            <Text style={styles.price}>{priceDisplay(deal.price)}</Text>
+          </View>
         </View>
       </View>
     );
@@ -26,6 +28,32 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: 150,
+    backgroundColor: '#ccc',
+  },
+  item: {
+    paddingHorizontal: 25,
+    paddingTop: 20,
+    borderStyle: 'solid',
+  },
+  info: {
+    padding: 10,
+    backgroundColor: '#fff',
+    borderColor: '#bbb',
+    borderWidth: 1,
+    borderTopWidth: 0,
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+  cause: {
+    flex: 2,
+  },
+  price: {
+    flex: 1,
+    textAlign: 'right',
   },
 });
 
